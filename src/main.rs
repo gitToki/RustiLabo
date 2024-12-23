4ccc4cf8e01c4b8516f41be9c145ff0afd70fbb9
Yassine Ferhane <yassineferhane69001@homail.com>
2024-12-23 19:18:35 +0100

    add of Reetrency attack

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,52 @@ use eframe::egui;
 
 fn main() -> Result<(), eframe::Error> {
     eframe::run_native(
-        "Rustilabo",
+        "RustiLabo",
         eframe::NativeOptions::default(),
         Box::new(|_cc| Box::new(MyApp::default())),
     )
 }
 
-struct MyApp;
+struct MyApp {
+    cleared: bool,
+}
 
 impl Default for MyApp {
     fn default() -> Self {
-        MyApp
+        MyApp {
+            cleared: false,
+        }
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
+            if self.cleared {
+                return;
+            }
 
-            ui.label(egui::RichText::new("          Toutes les failles de sécurité disponibles : ")
+            // Centrer "RustiLabo" horizontalement en haut
+            ui.horizontal(|ui| {
+                ui.centered_and_justified(|ui| {
+                    ui.label(egui::RichText::new("RustiLabo")
+                        .heading()
+                        .color(egui::Color32::LIGHT_YELLOW)
+                        .size(60.0));
+                });
+            });
+
+            ui.label(egui::RichText::new("All security vulnerabilities covered : ")
                 .color(egui::Color32::WHITE)
-                .size(30.0));
+                .size(20.0));
+
+            let button_size = egui::vec2(ctx.screen_rect().width() * 0.4, ctx.screen_rect().height() * 0.1);
+            if ui.add_sized(button_size, egui::Button::new("Reentrancy Attack")).clicked() {
+                self.cleared = true;
+            }
         });
     }
 }
+
+
 
